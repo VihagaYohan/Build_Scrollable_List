9ed435a313f872761fd91c75d8440798792acfbb
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-01-04 00:30:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/build_scrollable_list/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.build_scrollable_list
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.build_scrollable_list.ui.theme.Build_Scrollable_ListTheme
+import com.example.build_scrollable_list.model.Affirmation
+import androidx.compose.ui.platform.LocalContext
+import com.example.build_scrollable_list.data.Datasource
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Build_Scrollable_ListTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AffirmationApp() {
+    AffirmationList(affirmationList = Datasource().loadAffirmations())
+}
+
+@Composable
+fun AffirmationList(affirmationList: List<Affirmation>,
+                    modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(affirmationList) { affirmation ->
+            AffirmationCard(affirmation = affirmation,
+                modifier = Modifier.padding(8.dp))
+        }
+    }
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation,
+                    modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column(modifier = modifier) {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(id = affirmation.stringResourceId),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop)
+            Text(
+                text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AffirmationPreview() {
+    Build_Scrollable_ListTheme {
+        AffirmationCard(Affirmation(R.string.affirmation1, R.drawable.image1))
+    }
+}
